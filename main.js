2c243d10b93e4fe6ba337d94da3288583ce2f688
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-07-19 15:57:34 -0400

    finished with drop down list and information display. Need to work on styling next.

--- main.js
+++ main.js
@@ -2,15 +2,13 @@
 //basic: https://api.thecatapi.com/v1/images/search?api_key=YOUR_API_KEY
 //api key: d6UMByrl4eANDsX8UCdJzCqwHeH7zcdKlejRIUiVidyBfILxCyzTfjfMFsEyPhiI
 
-const baseURL = 'https://api.thecatapi.com/v1/images/search?limit=10'
+const baseURL = 'https://api.thecatapi.com/v1/images/search?'
 const apiKey = 'd6UMByrl4eANDsX8UCdJzCqwHeH7zcdKlejRIUiVidyBfILxCyzTfjfMFsEyPhiI';
 
 let welcEl = document.getElementsByClassName('welcome-header');
 let mainFormEl = document.getElementById('main-form');
 let petDisplayEl = document.querySelector('.pet-display-container');
 let mainContainerEl = document.querySelector('.form-container');
-
-
 
 document.querySelector('.cat-selection').addEventListener('change', getSelectedValue);
 function getSelectedValue(e)
@@ -24,89 +22,45 @@ function getSelectedValue(e)
 
     data.then((result) => {
         console.log(result);
-    })
-}
 
-document.querySelector('.checkSelectionCat').addEventListener('click', getValue);
-document.querySelector('.checkSelectionDog').addEventListener('click',getValue);
-function getValue(e) {
+        result.map((item) => {
+            console.log(item);
+            const imgEl = document.createElement('img');
+            imgEl.classList.add('img-display');
+            imgEl.setAttribute('src', `${item.url}`);
+            imgEl.setAttribute('width', '300px');
 
-    let value = "dogs";
+            //append image to the div element as a child
+            petDisplayEl.append(imgEl);
 
-    if(e.currentTarget.checked === true && e.target.value === 'cats')
-    {
-        e.target.disabled = true;
-        value = e.target.value;
-        console.log(`you picked ${value}`);
-        //where catAPI will be
-    } else
-    {
-        e.target.disabled = true;
-        console.log(`you picked ${value}`);
-        //where dogAPI will be
-    }
+            const originEl = document.createElement('p');
+            originEl.innerText = `Origin - ${item.breeds[0].origin}`;
 
-    processValue(value);
-}
+            const altNameEl = document.createElement('p');
+            altNameEl.innerText = `Alternative name - ${item.breeds[0].alt_names}`;
 
-function processValue(value)
-{
-    console.log(`processing ${value}`);
+            const labelsEl = document.querySelector('.labels');
+            labelsEl.append(originEl,altNameEl);
 
-    mainFormEl.addEventListener('submit', (e) => {
-        e.preventDefault();
 
-        if(value === 'cats')
-        {
-            console.log('you picked cats');
-            const resp = getCatAPI();
+            const descEl = document.createElement('p');
+            descEl.innerText = `Description - ${item.breeds[0].description}`;
 
-                resp.then((result) => {
-                console.log(result);
+            const wikiBtn = document.createElement('button');
+            wikiBtn.innerText = 'More information';
+            wikiBtn.setAttribute('onClick', 'getLink');
 
-                result.map((item) => {
-                const imgEl = document.createElement('img');
-                imgEl.classList.add('img-display');
-                imgEl.setAttribute('src', `${item.url}`);
-                imgEl.setAttribute('width', '300px');
+            petDisplayEl.append(labelsEl, descEl, wikiBtn);
 
-                petDisplayEl.append(imgEl);
-                })
-
+            petDisplayEl.classList.remove('toggle-display');
             })
 
 
-
-
-            // dataArr.map((item) => {
-
-            //     const imgEl = document.createElement('img');
-            //     imgEl.classList.add('img-display');
-            //     imgEl.setAttribute('src', `${item.url}`);
-            //     imgEl.setAttribute('width', '300px');
-
-            //     petDisplayEl.append(imgEl);
-
-
-            // })
-
-            // const imgEl = document.createElement('img');
-            // imgEl.setAttribute('src', `${dataArr[0].url}`);
-            // imgEl.setAttribute('width', '300px');
-
-            // document.querySelector('.pet-display-container').append(imgEl);
-            petDisplayEl.classList.remove('toggle-display');
-            petDisplayEl.classList.add('pet-display-container');
-            mainContainerEl.classList.remove('form-container');
-
-            document.querySelector('.main-page-img').classList.add('toggle-display');
-
-
-        } else
-        {
-            console.log('you picked dogs');
-        }
     })
+}
+
+const getLink = () => {
+    console.log('getting wiki link');
 }
 
 const getCatAPI = async (catBreed) => {
